b5acaa7603e9c7a0e8b2a19432fd889ea0dca91b
Zeerak Ahmed <zeerak.ahmed@gmail.com>
2017-11-19 02:54:22 -0500

    adding files

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+function startTimer() {
+
+}
+
+function stopTimer() {
+
+}
+
+function logData() {
+	
+}
+
+function showPostForm() {
+	startTimer()
+	document.getElementById('post-form').style.display = 'block'
+}
+
+function hidePostForm() {
+	stopTimer()
+	document.getElementById('post-form').style.display = 'none'
+}
