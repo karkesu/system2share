68b41aef18145886c2ced8d325da47bc9951d8db
karkesu <karkesu@gmail.com>
2017-11-21 16:43:57 -0500

    tasks

--- create_tasks.py
+++ create_tasks.py
@@ -11,8 +11,6 @@ def createHIT():
 #UPDATE THIS
     aws_access_key_id = 'AKIAJVHT5POJ4BDEGBNQ'
     aws_secret_access_key = '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'
-    debug = 1
-    reward = 0.05
     environments = {
       "live": {
           "endpoint": "mechanicalturk.sandbox.amazonaws.com",
@@ -32,11 +30,8 @@ def createHIT():
         aws_access_key_id=aws_access_key_id,
         aws_secret_access_key=aws_secret_access_key,
         host=mturk_environment['endpoint'])
-        # debug=debug)
 
-    external_question = ExternalQuestion('https://zeerak.net/279akz/getTask/', 500)
-    # external_question = ExternalQuestion('https://cs279-final-project.herokuapp.com/find', 500)
-    # question_html = open("templates/question.html", "r").read()
+    external_question = ExternalQuestion('https://zeerak.net/279akz/getTask/1/1', 500)
 
     response = mtc.create_hit(
         title='Share an article',
@@ -44,84 +39,23 @@ def createHIT():
         description='Read a short article. How would you share this on Facebook?',
         duration=120,
         question=external_question,
-        reward=reward,
-        # response_groups='',
+        reward=0.05,
+        # response_groups='', # batches??
         )
-    # The response included several fields that will be helpful later
-    print("HELLOOO==============3")
-    print(response)
-    print(str(response[0]))
+
     # The response included several fields that will be helpful later
     hit_type_id = response[0].HITTypeId
     hit_id = response[0].HITId
+    available_balance = mtc.get_account_balance()
+
+    print("-------------------------------------------------------")
     print("Your HIT has been created. You can see it at this link:")
-    print("https://workersandbox.mturk.com/mturk/preview?groupId={}".format(hit_type_id))
+    print(">>>>>> https://workersandbox.mturk.com/mturk/preview?groupId={}".format(hit_type_id))
     print("Your HIT ID is: {}".format(hit_id))
-    print("\nAnd see results here:")
-    print(mturk_environment['manage'])
-      #
-      # # client = boto.client(
-      # #     service_name='mturk',
-      # #     region_name='us-east-1',
-      # #     endpoint_url=mturk_environment['endpoint'],
-      # #     aws_access_key_id=aws_access_key_id,
-      # #     aws_secret_access_key=aws_secret_access_key,
-      # # )
-      #
-      # # The question we ask the workers is contained in this file.
-      # question_sample = open("questions.xml", "r").read()
-      #
-      # # # Use qualifications to only use Workers who have had at least 80% of their assignments approved.
-      # # worker_requirements = [{
-      # #     'QualificationTypeId': '000000000000000000L0',
-      # #     'Comparator': 'GreaterThanOrEqualTo',
-      # #     'IntegerValues': [80],
-      # #     'RequiredToPreview': True,
-      # # }]
-      #
-      # # Create the HIT
-      # response = client.create_hit(
-      #     MaxAssignments=3,
-      #     LifetimeInSeconds=600,
-      #     AssignmentDurationInSeconds=600,
-      #     Reward=mturk_environment['reward'],
-      #     Title='Share an article',
-      #     Keywords='read, react, share',
-      #     Description='Read a short article. How would you share this on Facebook?',
-      #     Question=ExternalQuestion("",500)
-      #     # QualificationRequirements=worker_requirements, #as a Turker I'm not qualified to see this HIT lol..
-      # )
-      #
-      # # theHIT = mtc.create_hit(question=q,
-      # #                     lifetime=10 * 60 * 60, # 10 hours
-      # #                     max_assignments=3,
-      # #                     title=title,
-      # #                     description=description,
-      # #                     keywords=keywords,
-      # #                     qualifications=qualifications,
-      # #                     reward=pay,
-      # #                     duration=120 * 60, # 120 minutes
-      # #                     approval_delay=5 * 60 * 60, # 5 hours
-      # #                     annotation=experimentName)
-      #
-      # # assert(theHIT.status == True)
-      # # print theHIT
-      # # print theHIT[0].HITId
-
-
+    print("And see results here:")
+    print(">>>>>> {}".format(mturk_environment['manage']))
     # In Sandbox this always returns $10,000. In live, it will be your acutal balance.
-    # print("Your account balance is {}".format(client.get_account_balance()['AvailableBalance']))
+    print("Your account balance is {}".format(available_balance))
+    print("-------------------------------------------------------")
 
-    # The response included several fields that will be helpful later
-    # hit_type_id = response['HIT']['HITTypeId']
-    # hit_id = response['HIT']['HITId']
-    # print("\nCreated HIT: {}".format(hit_id))
-
-    # print("\nYou can work the HIT here:")
-    # print(mturk_environment['preview'] + "?groupId={}".format(hit_type_id))
-    #
-    # print("\nAnd see results here:")
-    # print(mturk_environment['manage'])
-
-
-# PostHits()
+    return
--- server.py
+++ server.py
@@ -1,40 +1,70 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, make_response
 import sys
-from create_tasks import createHIT
-from setup import experimentSetup
+from create_tasks import * #createHIT
+# from setup import experimentSetup
 
+#This allows us to specify whether we are pushing to the sandbox or live site.
+DEV_ENVIROMENT_BOOLEAN = True
+if DEV_ENVIROMENT_BOOLEAN:
+    amazon_host = "https://workersandbox.mturk.com/mturk/externalSubmit"
+else:
+    amazon_host = "https://www.mturk.com/mturk/externalSubmit"
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-	createHIT()
-	return 'Hello, World!'
+	# userAgreement()
+	# createHIT()
+	return render_template("intro.html")
 
 @app.route('/logResults')
 def logResults():
 	# do something with results here
 	return 'LOG'
 
-@app.route('/getTask', methods=['GET','POST'])
+@app.route('/getTask/<articleID>/<annotationID>', methods=['GET','POST']) #('/getTask/<taskID>', methods=['GET','POST'])
 def getHIT():
-	if request.args.get('assignmentId') == "ASSIGNMENT_ID_NOT_AVAILABLE":
-		pass # worker hasn't accepted task yet
-	else:
-		render_data = {
-			"workerID": request.args.get('workerId'),
-			"assignmentID": request.args.get('assignmentId'),
-			"hitID": request.args.get('hitId'),
-			"turkSubmitTo": request.args.get('turkSubmitTo'),
-			"workerID": request.args.get('workerId'),
-			"article": getArticle()
-		}
+	# if request.args.get('assignmentId') == "ASSIGNMENT_ID_NOT_AVAILABLE":
+	# 	pass # worker hasn't accepted task yet
+	# else:
+	# 	pass
 
-		return render_template('task.html', data=render_data) # article=article)
-	return
+    # TO DO
+    # define article topic, annotation type, article.
+	data = {
+		"amazon_host": amazon_host,
+		"hitID": request.args.get('hitId'),
+		"workerID": request.args.get('workerId'),
+		"assignmentID": request.args.get('assignmentId'),
+		"turkSubmitTo": request.args.get('turkSubmitTo'),
+		"workerID": request.args.get('workerId'),
+		# "article_topic": article_topic,
+		# "annotation_type": annotation,
+		"article": getArticle(articleID)
+        "annotation": annotationID
+	}
+    # log_task(data)
+	response = make_response(render_template("task.html", data = data))
 
-def getArticle():
+    # #This is particularly nasty gotcha.
+    # #Without this header, your iFrame will not render in Amazon
+    # response.headers['x-frame-options'] = 'this_can_be_anything'
+
+	return response # article=article)
+
+def getArticle(articleID):
 
 	f = open('static/articles/test.txt', 'r')
 	data = f.readlines()
 	f.close()
 	return data
+
+# # Log before complete?
+# def log_task(data):
+#     hitID =data['hitId']
+#     workerID =data['workerId']
+#     assignmentID =data['assignmentId']
+#     turkSubmitTo =data['turkSubmitTo']
+#     workerID =data['workerId']
+#     # TBContd
+#     return
