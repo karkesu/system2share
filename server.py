219377d289ea68c74e7e5918fc322dcd5a1cdc2d
Zeerak Ahmed <zeerak.ahmed@gmail.com>
2017-11-20 22:13:20 -0500

    parse url parameters in gettask

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,12 +11,15 @@ def logResults():
 	# do something with results here
 	return 'LOG'
 
-@app.route('/getTask/')
+@app.route('/getTask')
 def getHIT():
+	assignmentID = request.args.get('assignmentId')
+	hitID = request.args.get('hitId')
+	turkSubmitTo = request.args.get('turkSubmitTo')
+	workerID = request.args.get('workerId')
 	article = getArticle()
 	return render_template('task.html', 
 							article=article)
-
 
 def getArticle():
 	f = open('static/articles/test.txt', 'r')
