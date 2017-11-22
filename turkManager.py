1b2d8edac1b2ba262e6bed7a8c978bd6b7944134
Zeerak Ahmed <zeerak.ahmed@gmail.com>
2017-11-22 02:03:17 -0500

    turkmanager improvements/settings

--- turkManager.py
+++ turkManager.py
@@ -11,7 +11,7 @@ AWS_SECRET_ACCESS_KEY = os.environ['SECRET_ACCESS_KEY']
 
 environments = {
   "live": {
-      "endpoint": "mechanicalturk.sandbox.amazonaws.com",
+      "endpoint": "mechanicalturk.amazonaws.com",
       "preview": "https://www.mturk.com/mturk/preview",
       "manage": "https://requester.mturk.com/mturk/manageHITs",
   },
@@ -22,13 +22,12 @@ environments = {
   },
 }
 
-mturk_environment = environments["live"] if IS_DEV_ENVIRONMENT else environments["sandbox"]
+mturk_environment = environments['sandbox'] if IS_DEV_ENVIRONMENT else environments['live']
 
 mtc = MTurkConnection(
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     host=mturk_environment['endpoint'])
-
 
 def createHIT(articleCategory, articleID):
 
@@ -74,6 +73,26 @@ def createManyHITs():
     for i in range(1,2):
         createHIT('education-system', str(i))
 
-def deleteAllHITs():
+def getAllHITIDs():
+    ids = []
     for hit in list(mtc.get_all_hits()):
-        mtc.disable_hit(hit.HITId)
+        ids.append(hit.HITId)
+    return ids
+
+def deleteAllHITs():
+    for hit in getAllHITIDs():
+        mtc.disable_hit(hit)
+
+def getAllAssignments(hitID):
+    assignments = []
+    page = 1
+    while len(mtc.get_assignments(hitID, page_number=str(page))) > 0:
+        assignments.extend(mtc.get_assignments(hit, page_number=str(page)))
+        page += 1
+    return assignments
+
+def getAllResponses():
+    responses = []
+    for hit in getAllHITIDs():
+        responses.append(getAllAssignments(hit))
+    return responses
